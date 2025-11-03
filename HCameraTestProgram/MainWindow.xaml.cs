2b659949897d1814c8897a9758478980eb484f69
zoohoon <wp0402401@naver.com>
2025-11-03 16:56:37 +0900

    테스트 커밋

--- HCameraTestProgram/MainWindow.xaml.cs
+++ HCameraTestProgram/MainWindow.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using FTech_CoaxlinkEx;  // Euresys SDK Wrapper
 
+// Test할꺼야
+
 namespace HCameraTestProgram
 {
     public partial class MainWindow : Window
